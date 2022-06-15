eb47f03d653f10c84c495cd8e6ec5fdde3897d3a
Tien Nam Dao <tien.dao2@tiki.vn>
2022-06-15 19:00:03 +0700

    feat: add copy feature

--- components/AddressConverter.js
+++ components/AddressConverter.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import { useCallback, useState } from "react";
+import { Box, TextField, Tooltip, Typography } from "@mui/material";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { astraToEth, ethToAstra } from "asa-address-converter";
@@ -21,12 +21,25 @@ const AddressConverter = () => {
 			toast(err.message);
 		}
 	};
+
+	const onCopy = useCallback(async () => {
+		try {
+			await navigator.clipboard.writeText(result);
+			toast('Copy successfully');
+		} catch (err) {
+			console.log(err);
+		}
+	}, [result]);
+
 	return (
 		<Box title="Address Converter" flexDirection="column">
 			<Typography variant="h4" component="div">Address Converter</Typography>
+			<div style={{height: 20}} />
 			<TextField style={{width: 500}} onChange={onInputChange} label="Input Address" variant="standard" />
 			<div style={{height: 20}} />
-			<TextField disabled style={{width: 500}} label="Converter Address" value={result} variant="standard" />
+			<Tooltip title="Click to copy">
+				<TextField disabled style={{width: 500}} label="Converter Address" value={result} variant="standard" onClick={onCopy}/>
+			</Tooltip>
 			<ToastContainer />
 		</Box>
 	);
